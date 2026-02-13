2a76b42db1b5b009429cec31b6afd7ed396ed46a
yahya zarred <yahyazarred1234@gmail.com>
2026-02-13 15:40:46 +0100

    minor fix

--- frontend/SCRUM plateform/src/App.tsx
+++ frontend/SCRUM plateform/src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Signup from "./pages/Signup";
 import Verify from "./pages/Verify";
